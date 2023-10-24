0dc90b1360d51339caf49e81a259c6c00658ba52
Sandstad17 <90138783+Sandstad17@users.noreply.github.com>
2023-10-24 16:27:54 +0200

    fiksa sånn at listview ikke er i nykontakt

--- app/src/main/java/com/example/mysms_s364752_s364744/NykontaktActivity.java
+++ app/src/main/java/com/example/mysms_s364752_s364744/NykontaktActivity.java
@@ -32,7 +32,6 @@ public class NykontaktActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nykontakt_activity);
-        //
 
         //Til å åpne SQLite
         dataKilde = new AvtaleAppDataKilde(this);
